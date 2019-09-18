edf1ed2dd550757e7dabc5bffba220c5dd354ee3
Jason Shepherd <jason_b_shepherd@acm.org>
2019-09-18 10:43:02 -0500

    Initial implementation: init/start/stop

---
+++ trolog/config.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+import shutil
+
+DEFAULT_LOCAL_CONFIG = '.trolog'
+
+class Config(object):
+    def __init__(self):
+        self.path = Path(os.environ['HOME'], DEFAULT_LOCAL_CONFIG)
+        self.active_path = Path(self.path, 'active-timers')
+        self.finished_path = Path(self.path, 'finished-timers')
+
+
+    def exists(self):
+        return self.path.exists() and self.active_path.exists() \
+            and self.finished_path.exists()
+
+
+    def init(self):
+        if self.exists():
+            shutil.rmtree(str(self.path))
+
+        self.path.mkdir()
+        self.active_path.mkdir()
+        self.finished_path.mkdir()
+
+
+    def wipe(self):
+        if self.exists():
+            shutil.rmtree(str(self.path))
---
+++ trolog/timer.py
@@ -0,0 +1,66 @@
+__all__ = ['Timers', 'TimerException']
+
+from pathlib import Path
+import time
+
+
+def valid_label(label):
+    return '/' not in label
+
+
+class TimerException(Exception):
+    pass
+
+
+class Timers(object):
+    def __init__(self, config):
+        self.config = config
+
+
+    def start(self, label):
+        if not valid_label(label):
+            raise TimerException('Illegal character found in label.')
+
+        label_fp = Path(self.config.active_path, label + '.txt')
+        if label_fp.exists():
+            raise TimerException(
+                'Timer for "{}" already started.'.format(label))
+        
+        with open(str(label_fp), 'w') as wf:
+            wf.write(str(time.time()) + '\n')
+
+
+    def stop(self, label):
+        if not valid_label(label):
+            raise TimerException('Illegal character found in label.')
+
+        label_fp = Path(self.config.active_path, label + '.txt')
+        if not label_fp.exists():
+            raise TimerException(
+                'Timer for "{}" was never started.'.format(label))
+
+        with open(str(label_fp), 'r') as rf:
+            start_time = float(rf.read().rstrip())
+            end_time = time.time()
+            delta = end_time - start_time
+
+            finished_fp = Path(self.config.finished_path, label + '.txt')
+            with open(str(finished_fp), 'a') as wf:
+                wf.write('{}:{}:{}\n'.format(start_time, end_time, delta))
+
+        label_fp.unlink()
+
+
+# active-timers
+#  `-- lbl1.txt
+#      --------
+#      time
+#
+# finished-timers
+#  `-- lbl2.txt
+#  |   --------
+#  |   start:end:delta
+#  |   start:end:delta
+#  `-- lbl3.txt
+#      --------
+#      start:end:delta
