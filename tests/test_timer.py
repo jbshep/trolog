3bca0558141e1ff14bf447a055d656e779363ca9
Jason Shepherd <jason_b_shepherd@acm.org>
2019-09-20 20:50:14 -0500

    coverage.py and more timer tests

--- tests/test_timer.py
+++ tests/test_timer.py
@@ -28,3 +28,24 @@ def test_fail_nostart_on_stop(new_config):
     t.start('lbl1') 
     with pytest.raises(TimerException):
         t.stop('lbl2') 
+
+
+def test_fail_start_illegal_label(new_config):
+    t = Timers(new_config)
+    with pytest.raises(TimerException):
+        t.start('bogus/label')
+
+
+def test_fail_stop_illegal_label(new_config):
+    t = Timers(new_config)
+    with pytest.raises(TimerException):
+        t.stop('bogus/label')
+
+
+def test_labels(new_config):
+    t = Timers(new_config)
+    labels = ['a', 'b', 'c']
+    for label in labels:
+        t.start(label) 
+        t.stop(label) 
+    assert t.labels() == labels
